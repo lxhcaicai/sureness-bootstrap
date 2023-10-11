4a26cba85f2c6979d8c06b1adb22516515f7c6e3
lxhcaicai <2778763221@qq.com>
2023-10-11 20:38:50 +0800

    git init

---
+++ src/main/java/com/github/surenessbootstrap/SurenessBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.github.surenessbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurenessBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurenessBootstrapApplication.class, args);
+    }
+
+}
